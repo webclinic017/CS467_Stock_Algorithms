c82b7b4faabc955e7ebefb600f010da7d0fc6fda
Patrick Parks <parkspa@oregonstate.edu>
2022-01-24 11:01:05 -0600

    added app.py and index.html

--- WebsiteGUI/app.py
+++ WebsiteGUI/app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+import sys
+
+sys.path.insert(1, '../Test-and-Training-Dataset-Creation')
+sys.path.insert(1, '../Model-Training')
+sys.path.insert(1, '../Model-Deployment')
+
+import CreateTTData
+import Training
+import Deployment
+
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    dataText = CreateTTData.createttdata()
+    trainingText = Training.training()
+    deploymentText = Deployment.deployment()
+    return render_template("index.html",
+                           dataText=dataText, trainingText=trainingText,
+                           deploymentText=deploymentText)
+
 
 if __name__ == '__main__':
     app.run()
