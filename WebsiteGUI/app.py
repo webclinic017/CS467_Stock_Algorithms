fb3878534903638edd4c4d1e09c6c55a19fcddaf
Patrick Parks <parkspa@oregonstate.edu>
2022-01-25 05:49:55 -0600

    Finished troubleshooting some bugs dealing with double printing status to webgui status boxes

--- WebsiteGUI/app.py
+++ WebsiteGUI/app.py
@@ -1,43 +1,70 @@
 from flask import Flask, render_template
 import jyserver.Flask as jsf
 import sys
+import os
+import time
 
 sys.path.insert(1, '../Test-and-Training-Dataset-Creation')
 sys.path.insert(1, '../Model-Training')
 sys.path.insert(1, '../Model-Deployment')
 
+# Importing each python program and running them (this will change)
 import CreateTTData
 import Training
 import Deployment
 
-
 app = Flask(__name__)
 
 
-@jsf.use(app) # This is a module named jyserver that allows manipulation of DOM
+@jsf.use(
+    app)  # This is a module named jyserver that allows manipulation of DOM
 class Jyserverapp:
-    def __init__(self): # Create an __init__ method
-        self.tr_status = ""
-        self.tr_status_len = 0
+    def __init__(self):  # Create an __init__ method
+        self.status = ""
+        self.status_len = 0
+        self.filepath = ""
+        self.loop = True
 
-    def status_update(self): # Increment method for DOM
-        print("Updating")
-        f = open("../Model-Training/training_status.txt")
-        lines = f.readlines()
-        print("tr_status: %d", len(self.tr_status))
-        print("tr_status_len: %d", self.tr_status_len)
-        print("Lines length is: %d", len(lines))
-        # if it's new information then append it to the status
-        if len(lines) > self.tr_status_len:
-            for i in range(len(lines)):
-                if i > self.tr_status_len:
-                    print("HERE")
-                    self.tr_status += lines[i]
-                    self.js.document.getElementById("t_status").innerHTML =\
-                        self.tr_status
-            self.tr_status_len = len(lines)
-            self.js.document.getElementById("t_status").scrollTop = \
-                self.js.document.getElementById("t_status").scrollHeight
+    def status_update(self, status_box):  # Increment method for DOM
+        self.status = ""
+        self.status_len = 0
+        self.loop = True
+
+        # This checks to see which status box is being updated
+        if status_box == 't_status':
+            self.filepath = "../Model-Training/training_status.txt"
+
+        # This loop checks a given file for updates and puts contents to a
+        # specific status text box on the webpage
+        while self.loop:
+            f = open(self.filepath, "r")
+            lines = f.readlines()
+
+            # If it's new information then append it to the status
+            if len(lines) > self.status_len:
+                for i in range(len(lines)):
+
+                    # Check for file's change in number of lines
+                    if i >= self.status_len:
+                        self.status += lines[i]
+                        self.status_len += 1
+
+                        # Update the specified status through DOM manipulation
+                        self.js.document.getElementById(status_box).innerHTML \
+                            = self.status
+
+                    # Stop looping if Done is sent and reset everything
+                    if lines[i] == 'Done':
+                        open(self.filepath, 'w').close()
+                        self.status = ""
+                        self.status_len = 0
+                        self.loop = False
+
+                # Make status scroll box scroll to bottom as it is updated
+                self.js.document.getElementById(status_box).scrollTop = \
+                    self.js.document.getElementById(status_box).scrollHeight
+            f.close()
+
 
 @app.route('/')
 def index():  # put application's code here
@@ -45,8 +72,9 @@ def index():  # put application's code here
     training_text = Training.training()
     deployment_text = Deployment.deployment()
     return Jyserverapp.render(render_template("index.html",
-                           data_text=data_text, training_text=training_text,
-                           deployment_text=deployment_text))
+                                              data_text=data_text,
+                                              training_text=training_text,
+                                              deployment_text=deployment_text))
 
 
 if __name__ == '__main__':
