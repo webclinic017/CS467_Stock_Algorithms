88965379d431ee6a73b39b9f9e7ca25c3bf16131
Patrick Parks <parkspa@oregonstate.edu>
2022-01-26 07:49:15 -0600

    Still working on the training page, worked out a lot of bugs with making sure everything goes smoothly for the user and having proper status updates.  Need to work on storing user choices, so on a page reload or whatnot, the user choices can be repopulated while training

--- WebsiteGUI/app.py
+++ WebsiteGUI/app.py
@@ -4,7 +4,7 @@ import sys
 import os
 import time
 
-sys.path.insert(1, '../Test-and-Training-Dataset-Creation')
+sys.path.insert(1, '../Dataset-Creation')
 sys.path.insert(1, '../Model-Training')
 sys.path.insert(1, '../Model-Deployment')
 
@@ -21,49 +21,126 @@ app = Flask(__name__)
 class Jyserverapp:
     def __init__(self):  # Create an __init__ method
         self.status = ""
+        self.onPageReload = ""
         self.status_len = 0
         self.filepath = ""
         self.loop = True
+        self.training_status = False
+        self.dSelection = ""    # Storage variable for user dataset selection
+        self.mNameInput = ""    # Storage variable for user Name for model
 
-    def status_update(self, status_box):  # Increment method for DOM
+
+    # Updates text boxes with status and re-enables user input when 'Done' seen
+    def training_status_box_update(self, status_box, t_ds_select,
+                                   m_name_input, train_button):
+        self.onPageReload = ""
         self.status = ""
         self.status_len = 0
         self.loop = True
+        self.filepath = "../Model-Training/training_status.txt"
 
-        # This checks to see which status box is being updated
-        if status_box == 't_status':
-            self.filepath = "../Model-Training/training_status.txt"
+
+        # Check to see if training is already in progress and populate the
+        # status box (in-case the user refreshed the browser) and disable
+        # user input, if not clear the status box
+        if self.training_status:
+            print("IN IF STATMENT")
+            f = open(self.filepath, "r")
+            lines = f.readlines()
+            for i in range(len(lines)):
+                self.onPageReload += lines[i]
+            # Re-Update the specified status through DOM manipulation
+            self.js.document.getElementById(status_box).innerHTML \
+                = self.onPageReload
+            f.close()
+
+            # Disable user input on the training page, if a refreshed happend
+            # during training
+            self.disable_training_user_input(t_ds_select,
+                                            m_name_input, train_button)
+
+            # Repopulate Dataset Selection on page reload
+            self.js.document.getElementById(t_ds_select).value \
+                = self.dSelection
+
+            # Repopulate user defined Model Name on page reload
+            self.js.document.getElementById(m_name_input).value \
+                = self.mNameInput
+        else:
+            return None
 
         # This loop checks a given file for updates and puts contents to a
         # specific status text box on the webpage
         while self.loop:
             f = open(self.filepath, "r")
             lines = f.readlines()
-
+            f.close()
             # If it's new information then append it to the status
             if len(lines) > self.status_len:
                 for i in range(len(lines)):
-
                     # Check for file's change in number of lines
                     if i >= self.status_len:
                         self.status += lines[i]
                         self.status_len += 1
 
-                        # Update the specified status through DOM manipulation
-                        self.js.document.getElementById(status_box).innerHTML \
-                            = self.status
-
                     # Stop looping if Done is sent and reset everything
-                    if lines[i] == 'Done':
-                        open(self.filepath, 'w').close()
-                        self.status = ""
-                        self.status_len = 0
+                    if lines[i] == 'Training Complete':
+                        self.status += '\n.......End..........'
+
+                        # This deactivates the training complete line by
+                        # adding a new line, and also adds a
+                        # separator to the training status file to separate
+                        # training sessions
+                        print('MATCH FOUND }}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}')
+                        open(self.filepath, "w").close()
                         self.loop = False
+                        self.training_status = False
+                        self.enable_training_user_input(t_ds_select,
+                                   m_name_input, train_button)
+
+                # Update the specified status through DOM manipulation
+                self.js.document.getElementById(status_box).innerHTML \
+                    = self.status
 
                 # Make status scroll box scroll to bottom as it is updated
                 self.js.document.getElementById(status_box).scrollTop = \
                     self.js.document.getElementById(status_box).scrollHeight
-            f.close()
+
+    # These are functions for the training page
+
+    # Training Setters
+    # Disables training user input and stores inputted user choices
+    def disable_training_user_input(self, t_ds_select, m_name_input,
+                                    train_button):
+        self.training_status = True
+
+        # Store Choices THESE CHOICES NEED TO BE WRITTEN TO A FILE BECAUSE
+        # WHEN TRAIN IS CLICKED OR THE SCREEN REFRESHED ALL VARIABLES IN THE
+        # CLASS ARE REINITIALIZED 
+        if self.mNameInput == "" and self.dSelection == "":
+            self.mNameInput = self.js.document.\
+                getElementById(m_name_input).value
+            self.dSelection = self.js.document.\
+                getElementById(t_ds_select).value
+
+        print("mNameInput------------->",self.mNameInput)
+        print("dSelection------------->",self.dSelection)
+
+        self.js.document.getElementById(t_ds_select).disabled = True
+        self.js.document.getElementById(m_name_input).disabled = True
+        self.js.document.getElementById(train_button).disabled = True
+
+    # Enables training user input
+    def enable_training_user_input(self, t_ds_select, m_name_input,
+                                   train_button):
+        self.training_status = False
+        self.js.document.getElementById(t_ds_select).disabled = False
+        self.js.document.getElementById(m_name_input).disabled = False
+        self.js.document.getElementById(train_button).disabled = False
+
+    # Training Getters
+    def get_training_status(self):
+        return self.training_status
 
 
 @app.route('/dataset')
@@ -71,7 +148,7 @@ def dataset():  # put application's code here
     data_text = CreateTTData.createttdata()
     training_text = Training.training()
     deployment_text = Deployment.deployment()
-    return Jyserverapp.render(render_template("dataset.html.html",
+    return Jyserverapp.render(render_template("dataset.html",
                                               data_text=data_text,
                                               training_text=training_text,
                                               deployment_text=deployment_text))
@@ -79,13 +156,20 @@ def dataset():  # put application's code here
 
 @app.route('/training')
 def training():  # put application's code here
-    data_text = CreateTTData.createttdata()
-    training_text = Training.training()
-    deployment_text = Deployment.deployment()
+    # data_text = CreateTTData.createttdata()
+    # training_text = Training.training()
+    # deployment_text = Deployment.deployment()
+
+    # Get the list of availible datasets
+    dataset_filepath = "../Dataset-Creation/datasets_list.txt"
+    dataset_list = open(dataset_filepath, 'r')
+    lines = dataset_list.readlines()
+    dataset_list.close()
+    datasets = []
+    for i in range(len(lines)):
+        datasets.append(lines[i])
     return Jyserverapp.render(render_template("training.html",
-                                              data_text=data_text,
-                                              training_text=training_text,
-                                              deployment_text=deployment_text))
+                                              datasets=datasets))
 
 
 @app.route('/backtest')
