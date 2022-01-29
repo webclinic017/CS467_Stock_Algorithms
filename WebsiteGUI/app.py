09d5b1606d38b0f770f02dad107df6df8cf67064
Patrick Parks <parkspa@oregonstate.edu>
2022-01-29 10:46:51 -0600

    Got everything working pretty good on Dataset Genration Page, it can now refresh and repopulate while creating the dataset with the user selected choices. Everything is implemented so far except the cancel button.

--- WebsiteGUI/app.py
+++ WebsiteGUI/app.py
@@ -20,21 +20,28 @@ app = Flask(__name__)
     app)  # This is a module named jyserver that allows manipulation of DOM
 class Jyserverapp:
     def __init__(self):  # Create an __init__ method
+        # Training Variables
         self.training_box_status = ""
+        self.training_dataset = ""  # Dataset User Selected to train on
+        self.model_name = ""  # Storage variable for user Name for model
         self.training_status_len = 0
         self.on_training_page_reload = ""
         self.training_loop = True
         self.training_status = False
-        self.training_dataset = ""  # Dataset User Selected to train on
-        self.model_name = ""  # Storage variable for user Name for model
 
-        self.dataset_box_status = ""
+        # Dataset Variables
+        self.dataset_d_status = ""
+        self.dataset_d_name_input = ""  # Storage variable for database name
+        self.dataset_d_etf_select = ""  # ETF the user selected for the dataset
+        self.dataset_checkbox_putt_call = "False"
+        self.dataset_checkbox_junk_bond_demand = "False"
+        self.dataset_checkbox_mcclellan_summation_index = "False"
+        self.dataset_checkbox_lag_time = "False"
+        self.dataset_lag_time_select = ""  # This depends on checkbox lag time
         self.dataset_status_len = ""
         self.on_dataset_page_reload = ""
         self.dataset_loop = True
         self.dataset_generation_status = False
-        self.dataset_etf = ""  # ETF the user selected for dataset generation
-        self.dataset_name = ""  # Storage variable for user Name for model
 
     # -------- The methods below are for the Training page ---------
     # Updates text boxes with status and re-enables user input when 'Done' seen
@@ -164,10 +171,18 @@ class Jyserverapp:
 
     # -------- The methods below are for the Dataset Generation page ---------
     # Updates text boxes with status and re-enables user input when 'Done' seen
-    def dataset_status_box_update(self, status_box, t_ds_select,
-                                  m_name_input, train_button):
+    def dataset_status_box_update(self, d_status,
+                                  d_name_input,
+                                  d_etf_select,
+                                  checkbox_putt_call,
+                                  checkbox_junk_bond_demand,
+                                  checkbox_mcclellan_summation_index,
+                                  checkbox_lag_time,
+                                  lag_time_select,
+                                  gen_dataset_button):
+
         self.on_dataset_page_reload = ""
-        self.dataset_box_status = ""
+        self.dataset_d_status = ""
         self.dataset_status_len = 0
         self.dataset_loop = True
         dataset_status_filepath = "../Dataset-Creation/dataset_status.txt"
@@ -180,19 +195,70 @@ class Jyserverapp:
             lines = f.readlines()
             for i in range(len(lines)):
                 self.on_dataset_page_reload += lines[i]
+
             # Re-Update the specified status through DOM manipulation
-            self.js.document.getElementById(status_box).innerHTML \
+            self.js.document.getElementById(d_status).innerHTML \
                 = self.on_dataset_page_reload
             f.close()
 
+            # Repopulate user defined input on page reload
+            # Database Name
+            self.js.document.getElementById(d_name_input).value \
+                = self.dataset_d_name_input
+
+            # ETF the user selected
+            self.js.document.getElementById(d_etf_select).value \
+                = self.dataset_d_etf_select
+
+            # The checkbox for the putt/call option
+
+            if self.dataset_checkbox_putt_call:
+                self.js.document.getElementById(checkbox_putt_call).checked \
+                    = True
+            else:
+                self.js.document.getElementById(checkbox_putt_call).checked \
+                    = False
+
+            # The checkbox for the junk bond demand option
+            if self.dataset_checkbox_junk_bond_demand:
+                self.js.document.getElementById(checkbox_junk_bond_demand). \
+                    checked = True
+            else:
+                self.js.document.getElementById(checkbox_junk_bond_demand). \
+                    checked = False
+
+            # The checkbox for the mcclellan summation index option
+            if self.dataset_checkbox_mcclellan_summation_index:
+                self.js.document.\
+                    getElementById(checkbox_mcclellan_summation_index).checked\
+                    = True
+            else:
+                self.js.document.\
+                    getElementById(checkbox_mcclellan_summation_index).checked\
+                    = False
+
+            # The checkbox for the lag time option
+            if self.dataset_checkbox_lag_time:
+                self.js.document.getElementById(checkbox_lag_time).checked \
+                    = True
+                self.js.document.getElementById(lag_time_select).value = \
+                    self.dataset_lag_time_select
+                print("LAG TIME SELECT: ", self.dataset_lag_time_select)
+            else:
+                self.js.document.getElementById(checkbox_lag_time).checked \
+                    = False
+
             # Disable user input on the dataset page, if a refreshed happened
             # during dataset generation
-            self.disable_dataset_user_input(t_ds_select,
-                                            m_name_input, train_button)
+            self.disable_dataset_user_input(d_name_input,
+                                            d_etf_select,
+                                            checkbox_putt_call,
+                                            checkbox_junk_bond_demand,
+                                            checkbox_mcclellan_summation_index,
+                                            checkbox_lag_time,
+                                            lag_time_select,
+                                            gen_dataset_button)
 
-            # Repopulate user defined Model Name on page reload
-            self.js.document.getElementById(m_name_input).value \
-                = self.dataset_name
         else:
             return None
 
@@ -207,12 +273,12 @@ class Jyserverapp:
                 for i in range(len(lines)):
                     # Check for file's change in number of lines
                     if i >= self.dataset_status_len:
-                        self.dataset_box_status += lines[i]
+                        self.dataset_d_status += lines[i]
                         self.dataset_status_len += 1
 
                     # Stop looping if Done is sent and reset everything
                     if lines[i] == 'Dataset Generation Complete':
-                        self.dataset_box_status += '\n.......End..........'
+                        self.dataset_d_status += '\n.......End..........'
                         print("Dataset Stop MATCH FOUND ________________")
                         # This deactivates dataset generation complete line by
                         # adding a new line, and also adds a
@@ -221,35 +287,102 @@ class Jyserverapp:
                         open(dataset_status_filepath, "w").close()
                         self.dataset_loop = False
                         self.dataset_generation_status = False
-                        self.enable_dataset_user_input(t_ds_select,
-                                                       m_name_input,
-                                                       train_button)
+                        self.enable_dataset_user_input(
+                            d_name_input,
+                            d_etf_select,
+                            checkbox_putt_call,
+                            checkbox_junk_bond_demand,
+                            checkbox_mcclellan_summation_index,
+                            checkbox_lag_time,
+                            lag_time_select,
+                            gen_dataset_button)
 
                 # Update the specified status through DOM manipulation
-                self.js.document.getElementById(status_box).innerHTML \
-                    = self.dataset_box_status
+                self.js.document.getElementById(d_status).innerHTML \
+                    = self.dataset_d_status
 
                 # Make status scroll box scroll to bottom as it is updated
-                self.js.document.getElementById(status_box).scrollTop = \
-                    self.js.document.getElementById(status_box).scrollHeight
+                self.js.document.getElementById(d_status).scrollTop = \
+                    self.js.document.getElementById(d_status).scrollHeight
 
     # Store User's choices for dataset in a text file
-    def store_user_dataset_choices(self, status_box, t_ds_select,
-                                   m_name_input, train_button):
+    def store_user_dataset_choices(self,
+                                   d_status,
+                                   d_name_input,
+                                   d_etf_select,
+                                   checkbox_putt_call,
+                                   checkbox_junk_bond_demand,
+                                   checkbox_mcclellan_summation_index,
+                                   checkbox_lag_time,
+                                   lag_time_select,
+                                   gen_dataset_button):
 
         # Grab the data from the webpage
-        if self.js.document.getElementById(t_ds_select).value != 0:
-            self.dataset_name = str(self.js.document
-                                    .getElementById(m_name_input).value)
+
+        # Get the name of the dataset
+        self.dataset_d_name_input = str(self.js.document.
+                                        getElementById(d_name_input).value)
+
+        # Get the user selected ETF name
+        self.dataset_d_etf_select = str(self.js.document.
+                                        getElementById(d_etf_select).value)
+
+        # User Selected options section
+        # Putt/Call option
+        if self.js.document.getElementById(checkbox_putt_call).value \
+                == "True":
+            self.dataset_checkbox_putt_call = True
+        else:
+            self.dataset_checkbox_putt_call = False
+
+        # Junk bond demand option
+        if self.js.document.getElementById(checkbox_junk_bond_demand).value \
+                == "True":
+            self.dataset_checkbox_junk_bond_demand = True
+        else:
+            self.dataset_checkbox_junk_bond_demand = False
+
+        # Mcclellan Summation Index option
+        if self.js.document.getElementById(checkbox_mcclellan_summation_index)\
+                .value == "True":
+            self.dataset_checkbox_mcclellan_summation_index = True
+        else:
+            self.dataset_checkbox_mcclellan_summation_index = False
+
+        # Lag Time option - the user selected lag time will be saved
+        if self.js.document.getElementById(checkbox_lag_time) \
+                .value == "True":
+            self.dataset_checkbox_lag_time = True
+            self.dataset_lag_time_select = str(self.js.document\
+                .getElementById(lag_time_select).value)
+        else:
+            self.dataset_checkbox_lag_time = False
 
         # Set path to the dataset config file
         dataset_config_filepath = "../Dataset-Creation/dataset_config.txt"
 
         # Label each choice
-        dataset_name = "Dataset Name: " + self.model_name
+        dataset_name = "Dataset Name: " + self.dataset_d_name_input
+        etf_selected = "Etf Selected: " + self.dataset_d_etf_select[:-1]
+        putt_call_option = "Putt/Call: " + str(self.dataset_checkbox_putt_call)
+
+        junk_bond_demand_option = "Junk Bond Demand: " + \
+                                  str(self.dataset_checkbox_junk_bond_demand)
+
+        mcclellan_summation_index_option = \
+            "Mcclellan Summation Index: " + \
+            str(self.dataset_checkbox_mcclellan_summation_index)
+
+        if self.dataset_checkbox_lag_time:
+            lag_time_option = "Lag Time: " + str(self.dataset_lag_time_select)
+        else:
+            lag_time_option = "Lag Time: " + "False"
 
         # Put user choices into a list
-        lines_to_write = [dataset_name]
+        lines_to_write = [dataset_name, etf_selected, putt_call_option,
+                          junk_bond_demand_option,
+                          mcclellan_summation_index_option,
+                          lag_time_option]
 
         # Erase the file
         open(dataset_config_filepath, 'w').close()
@@ -260,31 +393,67 @@ class Jyserverapp:
             f.write(lines_to_write[i] + "\n")
         f.close()
 
-        # After user input has been stored called the update method
+        # After user input has been stored call the update method
         self.dataset_generation_status = True
-        self.dataset_status_box_update(status_box, t_ds_select,
-                                       m_name_input, train_button)
+        self.dataset_status_box_update(d_status,
+                                       d_name_input,
+                                       d_etf_select,
+                                       checkbox_putt_call,
+                                       checkbox_junk_bond_demand,
+                                       checkbox_mcclellan_summation_index,
+                                       checkbox_lag_time,
+                                       lag_time_select,
+                                       gen_dataset_button)
 
     # Dataset Setters
     # Disables dataset user input and stores inputted user choices
-    def disable_dataset_user_input(self, t_ds_select, m_name_input,
-                                   train_button):
-        self.js.document.getElementById(t_ds_select).disabled = True
-        self.js.document.getElementById(m_name_input).disabled = True
-        self.js.document.getElementById(train_button).disabled = True
+    def disable_dataset_user_input(self,
+                                   d_name_input,
+                                   d_etf_select,
+                                   checkbox_putt_call,
+                                   checkbox_junk_bond_demand,
+                                   checkbox_mcclellan_summation_index,
+                                   checkbox_lag_time,
+                                   lag_time_select,
+                                   gen_dataset_button):
+
+        self.js.document.getElementById(d_name_input).disabled = True
+        self.js.document.getElementById(d_etf_select).disabled = True
+        self.js.document.getElementById(checkbox_putt_call).disabled = True
+        self.js.document.getElementById(checkbox_junk_bond_demand).disabled = \
+            True
+        self.js.document.getElementById(checkbox_mcclellan_summation_index). \
+            disabled = True
+        self.js.document.getElementById(checkbox_lag_time).disabled = True
+        self.js.document.getElementById(lag_time_select).disabled = True
+        self.js.document.getElementById(gen_dataset_button).disabled = True
 
     # Enables dataset user input
-    def enable_dataset_user_input(self, t_ds_select, m_name_input,
-                                  train_button):
-        self.dataset_generation_status = False
-        self.js.document.getElementById(t_ds_select).disabled = False
-        self.js.document.getElementById(m_name_input).disabled = False
-        self.js.document.getElementById(train_button).disabled = False
+    def enable_dataset_user_input(self,
+                                  d_name_input,
+                                  d_etf_select,
+                                  checkbox_putt_call,
+                                  checkbox_junk_bond_demand,
+                                  checkbox_mcclellan_summation_index,
+                                  checkbox_lag_time,
+                                  lag_time_select,
+                                  gen_dataset_button):
+
+        self.js.document.getElementById(d_name_input).disabled = False
+        self.js.document.getElementById(d_etf_select).disabled = False
+        self.js.document.getElementById(checkbox_putt_call).disabled = False
+        self.js.document.getElementById(checkbox_junk_bond_demand).disabled = \
+            False
+        self.js.document.getElementById(checkbox_mcclellan_summation_index). \
+            disabled = False
+        self.js.document.getElementById(checkbox_lag_time).disabled = False
+        self.js.document.getElementById(lag_time_select).disabled = False
+        self.js.document.getElementById(gen_dataset_button).disabled = False
 
 
 @app.route('/dataset')
 def dataset():
-    # Get the list of availible datasets
+    # Get the list of 3x ETF's
     etf_filepath = "../Dataset-Creation/etf_list.txt"
     etf_list = open(etf_filepath, 'r')
     lines = etf_list.readlines()
